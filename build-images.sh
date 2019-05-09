7647857baca685d20a758da0b0f2f1bd6cdb07af
Ihor Okhrimenko <iokhrime@redhat.com>
2019-05-09 22:17:17 +0300

    Add draft yaml file for cubernetes deployment
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 WORK_DIR=$(pwd)
-
 FRONTEND_DIR="$WORK_DIR/frontend"
-FRONTEND_IMAGE_NAME="iokhrime/graduate-work-frontend:latest"
+FRONTEND_IMAGE_NAME="bujhtr5555/graduate-work-frontend:latest"
 
 BACKEND_DIR="$WORK_DIR/backend"
-BACKEND_IMAGE_NAME="iokhrime/graduate-work-backend:latest"
+BACKEND_IMAGE_NAME="bujhtr5555/graduate-work-backend:latest"
+
+echo "Login to docker"
+docker login -u $1 -p$2
 
 echo "Build 'Frontend' image"
 cd $FRONTEND_DIR
@@ -14,3 +16,7 @@ docker build -t $FRONTEND_IMAGE_NAME ./
 echo "Build 'Backend' image"
 cd $BACKEND_DIR
 docker build -t $BACKEND_IMAGE_NAME ./
+
+echo "Push images to dockerhub"
+docker push $FRONTEND_IMAGE_NAME
+docker push $BACKEND_IMAGE_NAME
